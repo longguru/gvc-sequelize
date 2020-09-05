62357a3d388a98dff6bf2d771e98543096d455dc
Lo Van Long <longlv@v-auto.com.vn>
2020-09-05 19:46:44 +0700

    Setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const db = require("./models");
+console.log(db);
+(async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+module.exports = db;
